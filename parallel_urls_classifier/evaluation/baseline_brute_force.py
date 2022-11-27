121c57e61102c635124c2e5ea08df2530f6970e6
Cristian García Romero <cgr71ii@gmail.com>
2022-11-27 22:38:19 +0000

    Refactoring baseline

--- parallel_urls_classifier/evaluation/baseline_brute_force.py
+++ parallel_urls_classifier/evaluation/baseline_brute_force.py
@@ -5,7 +5,6 @@ import os
 import sys
 import logging
 import argparse
-import itertools
 
 cdir = os.path.dirname(os.path.realpath(__file__))
 
@@ -28,7 +27,7 @@ def get_gs(file, lowercase=False):
             continue
 
         if lowercase:
-            line = line.lower()
+            line = [l.lower() for l in line.lower()]
 
         src_gs.add(line[0])
         trg_gs.add(line[1])
@@ -36,23 +35,132 @@ def get_gs(file, lowercase=False):
 
     return gs, src_gs, trg_gs
 
-def evaluate_pair(src_lang, trg_lang, src_url, trg_url, gs, print_pair=True):
-    src_url_tokenized = tokenizer.tokenize(src_url, check_gaps=False)
-    trg_url_tokenized = tokenizer.tokenize(trg_url, check_gaps=False)
-    parallel = 0
+def evaluate_pairs_m_x_n(src_lang, trg_lang, src_urls, trg_urls, gs, src_gs, trg_gs, print_pairs=True):
+    def evaluate(src_url, trg_url):
+        src_url_tokenized = tokenizer.tokenize(src_url, check_gaps=False)
+        trg_url_tokenized = tokenizer.tokenize(trg_url, check_gaps=False)
+        parallel = 0
 
-    # Replace and check
-    normalized_url = [trg_lang if token == src_lang else token for token in src_url_tokenized]
-    parallel = 1 if normalized_url == trg_url_tokenized else 0
-    pair = f"{src_url}\t{trg_url}"
+        # Replace and check
+        normalized_url = [trg_lang if token == src_lang else token for token in src_url_tokenized]
+        parallel = 1 if normalized_url == trg_url_tokenized else 0
+        pair = f"{src_url}\t{trg_url}"
 
-    if print_pair and parallel:
-        print(pair)
+        if print_pairs and parallel:
+            print(pair)
 
-    y_pred = parallel
-    y_true = 1 if pair in gs else 0
+        y_pred = parallel
+        y_true = 1 if pair in gs else 0
 
-    return y_pred, y_true
+        return y_pred, y_true
+
+    y_pred, y_true = [], []
+    matches = 0
+    total_pairs = 0
+
+    for src_url in src_urls:
+        remove_trg_urls_idx = None
+
+        for idx, trg_url in enumerate(trg_urls):
+            pair = False
+
+            if evaluate_urls_in_gs and gs_file:
+                if src_url in src_gs or trg_url in trg_gs:
+                    # Only append those URLs which are in the GS (we don't need to evaluate ALL the src and trg product URLs)
+                    pair = True
+            else:
+                pair = True
+
+            if pair:
+                # Evaluate URL pair
+                _y_pred, _y_true = evaluate(src_url, trg_url)
+
+                y_pred.append(_y_pred)
+                y_true.append(_y_true)
+
+                total_pairs += 1
+
+                if _y_pred:
+                    remove_trg_urls_idx = idx
+
+                    matches += 1
+
+                    break # src_url now has a match, so we don't look for any other
+
+        if remove_trg_urls_idx is not None:
+            # Remove trg URLs which were identified as parallel (we don't need to do the same for the src URLs since they will
+            #  be checked only once)
+            del trg_urls[remove_trg_urls_idx]
+
+    return y_pred, y_true, matches, total_pairs
+
+def evaluate_section_42(src_lang, trg_lang, src_urls, trg_urls, gs, src_gs, print_pairs=True):
+    trg_urls_tokenized = [tokenizer.tokenize(trg_url, check_gaps=False) for trg_url in trg_urls]
+
+    def evaluate(src_url):
+        src_url_tokenized = tokenizer.tokenize(src_url, check_gaps=False)
+        #trg_url_tokenized = tokenizer.tokenize(trg_url, check_gaps=False)
+        parallel = 0
+
+        # Replace and check
+        normalized_url = [trg_lang if token == src_lang else token for token in src_url_tokenized]
+        y_pred, y_true, trg_url_idx = None, None, None
+        parallel = 0
+        pair = ''
+
+        try:
+            trg_url_idx = trg_urls_tokenized.index(normalized_url)
+            parallel = 1
+            pair = f"{src_url}\t{trg_urls[trg_url_idx]}"
+        except ValueError:
+            # Normalized URL not found: is not a "possible pair"
+
+            return y_pred, y_true, trg_url_idx
+
+        if print_pairs and parallel:
+            print(pair)
+
+        y_pred = parallel
+        y_true = 1 if pair in gs else 0
+
+        return y_pred, y_true, trg_url_idx
+
+    y_pred, y_true = [], []
+    matches = 0
+    total_pairs = 0
+
+    for src_url in src_urls:
+        remove_trg_urls_idx = None
+        pair = False
+
+        if evaluate_urls_in_gs and gs_file:
+            if src_url in src_gs:
+                # Only append those URLs which are in the GS
+                pair = True
+        else:
+            pair = True
+
+        if pair:
+            # Evaluate URL pair
+            _y_pred, _y_true, remove_trg_urls_idx = evaluate(src_url)
+
+            if remove_trg_urls_idx is not None:
+                # We have a "possible pair"
+
+                y_pred.append(_y_pred)
+                y_true.append(_y_true)
+
+                total_pairs += 1
+
+                if _y_pred:
+                    # Remove trg URLs which were identified as parallel (we don't need to do the same for the src URLs
+                    #  since they will be checked only once)
+                    del trg_urls[remove_trg_urls_idx]
+                    del trg_urls_tokenized[remove_trg_urls_idx]
+
+                    matches += 1
+
+    return y_pred, y_true, matches, total_pairs
 
 def main(args):
     input_file = args.input
@@ -61,13 +169,10 @@ def main(args):
     gs_file = args.gold_standard
     evaluate_urls_in_gs = args.evaluate_urls_in_gs
     lowercase = args.lowercase
+    evaluate_m_x_n = args.evaluate_m_x_n
 
     gs, src_gs, trg_gs = get_gs(gs_file, lowercase=lowercase) if gs_file else (set(), set(), set())
-    y_true, y_pred = [], []
     src_urls, trg_urls = [], []
-    total_pairs = 0
-    src_urls_found, trg_urls_found = set(), set()
-    matches = 0
 
     # Read URLs
     for idx, line in enumerate(input_file, 1):
@@ -98,42 +203,25 @@ def main(args):
     else:
         logging.warning("GS will not be used for evaluation, so the product of all URLs will be added")
 
-    for src_url, trg_url in itertools.product(src_urls, trg_urls):
-        pair = False
+    logging.info("Evaluating...")
 
-        if src_url in src_urls_found or trg_url in trg_urls_found:
-            # We don't evaluate URLs which were classified as parallel
-            continue
+    # Evaluate
+    if evaluate_m_x_n:
+        y_pred, y_true, matches, total_pairs =\
+            evaluate_pairs_m_x_n(src_lang, trg_lang, src_urls, trg_urls, gs, src_gs, trg_gs)
+    else:
+        y_pred, y_true, matches, total_pairs =\
+            evaluate_section_42(src_lang, trg_lang, src_urls, trg_urls, gs, src_gs)
 
-        if evaluate_urls_in_gs and gs_file:
-            if src_url in src_gs or trg_url in trg_gs:
-                # Only append those URLs which are in the GS (we don't need to evaluate ALL the src and trg product URLs)
-                pair = True
-        else:
-            pair = True
-
-        if pair:
-            # Evaluate URL pair
-            _y_pred, _y_true = evaluate_pair(src_lang, trg_lang, src_url, trg_url, gs)
-
-            y_pred.append(_y_pred)
-            y_true.append(_y_true)
-
-            total_pairs += 1
-
-            if _y_pred:
-                src_urls_found.add(src_url)
-                trg_urls_found.add(trg_url)
-
-                matches += 1
-
+    # Some statistics
     negative_matches = total_pairs - matches
 
     logging.info("URL pairs: %d", total_pairs)
     logging.info("Positive and negative matches: %d, %d", matches, negative_matches)
-    logging.info("Evaluating...")
 
     if gs_file:
+        logging.info("Using GS in order to get some evaluation metrics...")
+
         # Log metrics
         confusion_matrix = sklearn.metrics.confusion_matrix(y_true, y_pred, labels=[0, 1])
         tn, fp, fn, tp = confusion_matrix.ravel()
@@ -160,6 +248,8 @@ def initialization():
     parser.add_argument('--gold-standard', type=argparse.FileType('rt'), help="GS filename with parallel URLs (TSV format)")
     parser.add_argument('--evaluate-urls-in-gs', action="store_true", help="Only evaluate those URLs which are present in the GS")
     parser.add_argument('--lowercase', action="store_true", help="Lowercase URLs (GS as well if provided). It might increase the evaluation results")
+    parser.add_argument('--evaluate-m-x-n', action="store_true",
+                        help="Evaluate all the possible pairs instead of construct 'possible pairs' like is described in section 4.2 of YODA system")
 
     parser.add_argument('-v', '--verbose', action="store_true", help="Verbose logging mode")
 
