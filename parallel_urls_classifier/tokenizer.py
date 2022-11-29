c482840d2bd7652625e108cd9e98fee736496a50
Cristian García Romero <cgr71ii@gmail.com>
2022-11-29 23:30:23 +0000

    Refactoring tokenizer

--- parallel_urls_classifier/tokenizer.py
+++ parallel_urls_classifier/tokenizer.py
@@ -2,6 +2,13 @@
 import os
 import sys
 import logging
+import argparse
+
+cdir = os.path.dirname(os.path.realpath(__file__))
+
+sys.path.insert(0, f"{cdir}/..")
+
+import parallel_urls_classifier.utils.utils as utils
 
 from nltk.tokenize import RegexpTokenizer
 
@@ -9,11 +16,20 @@ _tokenizer_regex = r'[^\W_0-9]+|[^\w\s]+|_+|\s+|[0-9]+' # Similar to wordpunct_t
 _tokenize = RegexpTokenizer(_tokenizer_regex).tokenize
 _tokenize_gaps = RegexpTokenizer(_tokenizer_regex, gaps=True).tokenize
 
-def tokenize(s, check_gaps=True, gaps_whitelist=[' ']):
-    tokenized_str = _tokenize(s)
+_tokenizers = {
+    "tokenizers": {
+        "wordpunct_tokenize_urls": _tokenize,
+    },
+    "gaps": {
+        "wordpunct_tokenize_urls": _tokenize_gaps
+    }
+}
+
+def tokenize(s, check_gaps=True, gaps_whitelist=[' '], tokenizer="wordpunct_tokenize_urls"):
+    tokenized_str = _tokenizers["tokenizers"][tokenizer](s)
 
     if check_gaps:
-        tokenized_str_gaps = _tokenize_gaps(s)
+        tokenized_str_gaps = _tokenizers["gaps"][tokenizer](s)
 
         if len(tokenized_str_gaps) != 0:
             for gap in tokenized_str_gaps:
@@ -25,8 +41,49 @@ def tokenize(s, check_gaps=True, gaps_whitelist=[' ']):
 
     return tokenized_str
 
-if __name__ == "__main__":
-    for s in sys.stdin.buffer:
+def main(args):
+    input_filename = args.input_filename
+    check_gaps = not args.disable_check_gaps
+    tokenizer = args.tokenizer
+    disable_join_tokens = args.disable_join_tokens
+
+    join_function = (lambda s: s) if disable_join_tokens else ' '.join
+    input_fd = sys.stdin.buffer
+
+    if input_filename != '-':
+        input_fd = open(input_filename, "rb")
+
+    for s in input_fd:
         s = s.decode(errors="backslashreplace")
 
-        print(' '.join(tokenize(s.rstrip('\n'), check_gaps=False if ("PUC_CHECK_GAPS" in os.environ and os.environ["PUC_CHECK_GAPS"] in ("False", "false")) else True)))
+        print(join_function(tokenize(s.rstrip('\n'), check_gaps=check_gaps, tokenizer=tokenizer)))
+
+    if input_filename != '-':
+        input_fd.close()
+
+def initialization():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     description="Tokenizer")
+
+    # We can't use argparse.FileType('rb') due to https://bugs.python.org/issue14156
+    #parser.add_argument('--input-filename', type=argparse.FileType('rb'), default='-', help="Input file with sentences to be tokenized")
+    parser.add_argument('--input-filename', default='-', help="Input file with sentences to be tokenized")
+    parser.add_argument('--disable-check-gaps', action="store_true", help="Do not check gaps when tokenize provided sentences")
+    parser.add_argument('--tokenizer', choices=["wordpunct_tokenize_urls"], default="wordpunct_tokenize_urls",
+                        help="Different available tokenizers")
+    parser.add_argument('--disable-join-tokens', action="store_true", help="Do not join the sentences with ' ' but show the list with the tokens")
+
+    parser.add_argument('-v', '--verbose', action="store_true", help="Verbose logging mode")
+
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == "__main__":
+    args = initialization()
+
+    utils.set_up_logging(level=logging.DEBUG if args.verbose else logging.INFO)
+
+    logging.debug("Arguments processed: {}".format(str(args)))
+
+    main(args)
