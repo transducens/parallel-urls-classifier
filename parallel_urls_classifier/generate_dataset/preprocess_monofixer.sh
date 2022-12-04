e3c24533243c4f4e9131cb6870e00c6ef2e2521b
Cristian García Romero <cgr71ii@gmail.com>
2022-12-04 10:49:07 +0000

    Minor change

--- parallel_urls_classifier/generate_dataset/preprocess_monofixer.sh
+++ parallel_urls_classifier/generate_dataset/preprocess_monofixer.sh
@@ -6,4 +6,7 @@ BIN_=$([[ -z "$3" ]] && echo "monofixer" || echo "$3")
 
 # If you want to execute bifixer, e.g.: bash $0 "en es" "--tcol 2" "bifixer"
 
-$BIN_ --scol 1 --ignore_long --ignore_html --ignore_duplicates --ignore_segmentation --ignore_empty -q $EXTRA_ARGS_ - - $LANG_
+#$BIN_ --scol 1 --ignore_long --ignore_html --ignore_duplicates --ignore_segmentation --ignore_empty -q $EXTRA_ARGS_ - - $LANG_
+
+# Similar to Bitextor:
+$BIN_ --scol 1 --ignore_duplicates -q $EXTRA_ARGS_ - - $LANG_
