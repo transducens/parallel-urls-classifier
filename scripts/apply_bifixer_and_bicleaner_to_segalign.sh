6d124eb8770baec6ebcb896ba19d0315cdaf544e
Cristian García Romero <cgr71ii@gmail.com>
2022-12-17 15:16:48 +0000

    Minor changes

--- scripts/apply_bifixer_and_bicleaner_to_segalign.sh
+++ scripts/apply_bifixer_and_bicleaner_to_segalign.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+#SBATCH --job-name=bicleaner_ai
+#SBATCH --gres=gpu:5
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=4G
+#SBATCH -e slurm-%j-apply_bifixer_and_bicleaner_to_segalign.err
+#SBATCH -o slurm-%j-apply_bifixer_and_bicleaner_to_segalign.out
 
 SEGALIGN_GLOB="$1"
 TRG_LANG="$2"
 BICLEANER_AI_MODEL_PATH="$3"
-BICLEANER_EXTRA_ARGS="$4"
-BICLEANER_PREFIX_CMD="$5"
+BICLEANER_EXTRA_ARGS="$4" # e.g. "-b 10000 --batch_size 64"
+PROFILING="$5" # e.g. "/usr/bin/time -v"
 
 # Expected format of the segalign files: ... <tab> ... <tab> trg_text <tab> src_text (en) [<tab> ...]
 
 if [[ -z "$SEGALIGN_GLOB" ]] || [[ -z "$TRG_LANG" ]] || [[ -z "$BICLEANER_AI_MODEL_PATH" ]]; then
   >&2 echo -n "Syntax: $(basename $0) <segalign_files_glob> <trg_lang> <bicleaner_ai_model_path>"
-  >&2 echo -n " [<bicleaner_extra_args> <bicleaner_prefix_cmd>]"
+  >&2 echo -n " [<bicleaner_extra_args> <profiling>]"
   >&2 echo ""
 
   exit 1
@@ -24,9 +30,9 @@ fi
 SUFFIX_BICLEANER=".bicleaner_scores_after_bifixer"
 APPLY_CMD="""\
 cut -f3,4 \
-  | bifixer -q --scol 1 --tcol 2 --ignore_empty --ignore_duplicates \
+  | $PROFILING bifixer -q --scol 1 --tcol 2 --ignore_empty --ignore_duplicates \
     --ignore_segmentation - - "$TRG_LANG" en \
-  | $BICLEANER_PREFIX_CMD cache -k 1,2 bicleaner-ai-classify -q --scol 2 --tcol 1 --score_only \
+  | $PROFILING cache -k 1,2 bicleaner-ai-classify -q --scol 2 --tcol 1 --score_only \
     --disable_porn_removal --disable_minimal_length $BICLEANER_EXTRA_ARGS \
     - - "$BICLEANER_AI_MODEL_PATH" \
 """
