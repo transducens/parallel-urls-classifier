b2e3a369ad66b75fd945adcafe892c9cc65e0a6d
Cristian García Romero <cgr71ii@gmail.com>
2022-12-17 13:41:43 +0000

    Minor change

--- scripts/apply_bifixer_and_bicleaner_to_segalign.sh
+++ scripts/apply_bifixer_and_bicleaner_to_segalign.sh
@@ -22,33 +22,38 @@ if [[ ! -d "$BICLEANER_AI_MODEL_PATH" ]]; then
 fi
 
 SUFFIX_BICLEANER=".bicleaner_scores_after_bifixer"
-OUTPUT_FILE="{}$SUFFIX_BICLEANER"
+APPLY_CMD="""\
+cut -f3,4 \
+  | bifixer -q --scol 1 --tcol 2 --ignore_empty --ignore_duplicates \
+    --ignore_segmentation - - "$TRG_LANG" en \
+  | $BICLEANER_PREFIX_CMD cache -k 1,2 bicleaner-ai-classify -q --scol 2 --tcol 1 --score_only \
+    --disable_porn_removal --disable_minimal_length $BICLEANER_EXTRA_ARGS \
+    - - "$BICLEANER_AI_MODEL_PATH" \
+"""
 
 if [[ "$SEGALIGN_GLOB" == "-" ]]; then
-  EVAL_CMD="cat"
-
   OUTPUT_FILE="$PWD/$(date +%Y%m%d%H%M%S%N)$SUFFIX_BICLEANER"
+  f="${OUTPUT_FILE}.gz";
 
-  >&2 echo "Output file: $OUTPUT_FILE[.*]"
+  >&2 echo "Output file: $f"
+
+  eval $APPLY_CMD \
+    | pigz -c > "$f"
 else
-  EVAL_CMD="ls $SEGALIGN_GLOB"
+  ls $SEGALIGN_GLOB \
+    | xargs -I{} -P1 bash -c \
+      ' \
+        f="{}'$SUFFIX_BICLEANER'.gz"; \
+        if [[ -f "$f" ]]; then \
+          echo "File already exists: $f"; \
+        else \
+          >&2 echo "Output file: $f";
+          zcat {} \
+            | eval '"'$APPLY_CMD'"' \
+            | pigz -c > "$f"; \
+        fi; \
+        nolines1=$(zcat "{}" | wc -l); \
+        nolines2=$(zcat "$f" | wc -l); \
+        if [[ "$nolines1" != "$nolines2" ]]; then echo "$nolines1 vs $nolines2 : {}"; fi \
+      '
 fi
-
-eval "$EVAL_CMD" \
-  | xargs -I{} -P1 bash -c \
-    ' \
-      f="'$OUTPUT_FILE'.gz"; \
-      if [[ -f "$f" ]]; then \
-        echo "File already exists: $f"; \
-      else \
-        zcat {} | cut -f3,4 | bifixer -q --scol 1 --tcol 2 --ignore_empty --ignore_duplicates \
-            --ignore_segmentation - - "'$TRG_LANG'" en \
-          | '$BICLEANER_PREFIX_CMD' cache -k 1,2 bicleaner-ai-classify -q --scol 2 --tcol 1 --score_only \
-            --disable_porn_removal --disable_minimal_length '$BICLEANER_EXTRA_ARGS' \
-            - - "'$BICLEANER_AI_MODEL_PATH'" \
-          | pigz -c > "$f"; \
-      fi; \
-      nolines1=$(zcat "{}" | wc -l); \
-      nolines2=$(zcat "$f" | wc -l); \
-      if [[ "$nolines1" != "$nolines2" ]]; then echo "$nolines1 vs $nolines2 : {}"; fi \
-    '
